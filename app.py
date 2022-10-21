45e3ae350f265270089e7177e8a074b892a45222
goncalo.a.santos@gmail.com <goncalo.a.santos@gmail.com>
2022-10-21 10:08:02 +0200

    attempting without csv storage

--- app.py
+++ app.py
@@ -10,9 +10,18 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask('API_newsfeedsPT')
 
 OUT_RESP = None
+OUT_RESP_DF = None
+
 
 def get_feed_jn():
+
+    global OUT_RESP
+    global OUT_RESP_DF
+
     print('Updating JN feed...')
+
+    ## Getting new data in dataframe format
+
     r = requests.get('https://ws.globalnoticias.pt/feed/jn/articles/ultimas/rss')
 
     resp_json = xmltodict.parse(r.text)
@@ -29,10 +38,24 @@ def get_feed_jn():
         }
         resp_json_sm.append(item_sm)
         
-    new_feed_df = pd.DataFrame.from_records(resp_json_sm)
 
-    try:
-        old_df = pd.read_csv('jn.csv')
+    if OUT_RESP:
+
+        new_feed_df = pd.DataFrame.from_records(resp_json_sm)
+
+        out_resp_json = []
+
+        for item in OUT_RESP:
+            item_json = {
+                'titulo': item['titulo'],
+                'link': item['link'],
+                'categoria': item['categoria'],
+                'pubDate': item['pubDate']
+            }
+            out_resp_json.append(item_json)
+        
+        old_df = pd.DataFrame.from_records(out_resp_json)
+    
         old_df.categoria = old_df.categoria.apply(str)
         new_feed_df.categoria = new_feed_df.categoria.apply(str)
 
@@ -40,15 +63,14 @@ def get_feed_jn():
         merged_df = merged_df.sort_values('pubDate', ascending=False).drop_duplicates(subset=['pubDate'])
         merged_df.categoria = merged_df.categoria.apply(lambda x: ast.literal_eval(x))
 
-        pd.DataFrame.to_csv(merged_df, 'jn.csv', index=False)
         out_resp = merged_df.to_dict(orient="records")
-    except FileNotFoundError:
-        pd.DataFrame.to_csv(new_feed_df, 'jn.csv', index=False)
+    
+    else:
         out_resp = resp_json_sm
 
     print('Updating JN completed...')
-    global OUT_RESP
     OUT_RESP = out_resp[:100]
+    print(OUT_RESP)
 
 @app.route('/get_jn')
 def start():
@@ -56,9 +78,11 @@ def start():
     print(OUT_RESP)
     return OUT_RESP
 
-if __name__ == '__main__' or __name__ == 'app':
-    get_feed_jn()
-    sched = BackgroundScheduler(daemon=True)
-    sched.add_job(get_feed_jn, 'interval', seconds=60)
-    sched.start()
-    atexit.register(lambda: sched.shutdown())
+get_feed_jn()
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(get_feed_jn, 'interval', seconds=10)
+sched.start()
+atexit.register(lambda: sched.shutdown())
+
+if __name__ == '__main__':
+    app.run()
